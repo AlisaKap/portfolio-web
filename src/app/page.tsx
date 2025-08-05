ebe74361308a50fc17e75e98c7e5cb65fa09e206
AlisaKap <148804600+AlisaKap@users.noreply.github.com>
2025-08-05 12:08:40 +0300

    update: illustrate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import Sidebar from '@/components/Universal/Sidebar/Sidebar';
 import Text from '@/components/Universal/Text/Text';
 import '@fontsource/ubuntu';
+import InProgress from "@/app/inProgress/page";
 
 export default function HomePage() {
 
     return (
         <main>
-            <h1>Добро пожаловать!</h1>
-            <p>Это главная страница.</p>
+            <InProgress />
         </main>
     );
 }
