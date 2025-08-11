5683af5ee1ab0a9b1c11e3da5738a5116eb95d33
AlisaKap <148804600+AlisaKap@users.noreply.github.com>
2025-08-11 10:48:34 +0300

    demo

--- src/app/illustrate/page.tsx
+++ src/app/illustrate/page.tsx
@@ -12,19 +12,19 @@ type ImageType = {
 };
 
 const images: ImageType[] = [
-    { src: '/images/illustrate/1.png', format: 'vertical' },
-    { src: '/images/illustrate/5.png', format: 'vertical' },
-    { src: '/images/illustrate/6.png', format: 'vertical' },
-    { src: '/images/illustrate/2.jpg', format: 'vertical' },
-    { src: '/images/illustrate/7.png', format: 'vertical' },
-    { src: '/images/illustrate/8.png', format: 'vertical' },
-    { src: '/images/illustrate/9.jpg', format: 'vertical' },
-    { src: '/images/illustrate/3.jpg', format: 'vertical' },
-    { src: '/images/illustrate/4.png', format: 'vertical' },
-    { src: '/images/illustrate/10.png', format: 'horizontal' },
-    { src: '/images/illustrate/13.png', format: 'horizontal' },
-    { src: '/images/illustrate/11.png', format: 'horizontal' },
-    { src: '/images/illustrate/12.png', format: 'horizontal' },
+    { src: '/images/Illustrate/1.png', format: 'vertical' },
+    { src: '/images/Illustrate/5.png', format: 'vertical' },
+    { src: '/images/Illustrate/6.png', format: 'vertical' },
+    { src: '/images/Illustrate/2.jpg', format: 'vertical' },
+    { src: '/images/Illustrate/7.png', format: 'vertical' },
+    { src: '/images/Illustrate/8.png', format: 'vertical' },
+    { src: '/images/Illustrate/9.jpg', format: 'vertical' },
+    { src: '/images/Illustrate/3.jpg', format: 'vertical' },
+    { src: '/images/Illustrate/4.png', format: 'vertical' },
+    { src: '/images/Illustrate/10.png', format: 'horizontal' },
+    { src: '/images/Illustrate/13.png', format: 'horizontal' },
+    { src: '/images/Illustrate/11.png', format: 'horizontal' },
+    { src: '/images/Illustrate/12.png', format: 'horizontal' },
 ];
 
 export default function Page() {
